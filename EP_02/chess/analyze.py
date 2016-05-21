a3f3c184c8a9a6a9be522db808cbf879b2c42bab
Larissa13 <lariop13@gmail.com>
2016-05-21 20:23:57 -0300

    EP_02 part1

--- EP_02/chess/analyze.py
+++ EP_02/chess/analyze.py
@@ -4,14 +4,30 @@ from vpi.io import read_gray_image, display_image
 
 def analyze_chess_game(image_directory):
     game = Game()
-    boards = []
+    plays = []
+    board = Chessboard()
+    board.print_board()
     for i in range(28):
         image = str(image_directory) + "/" + str (i) + ".png"
         f = read_gray_image(image)
-        boards.append(f)
+        plays.append(f)
     
-    n = len(boards)
+    n = len(plays)
     for i in range(n-1):
-        diff = boards[i+1] - boards[i]
+        diff = plays[i+1] - plays[i]
+        moved = np.nonzero(diff)
+        xmin, ymin = (moved[0].min(0)/60).astype(int), (moved[1].min(0)/60).astype(int)
+        xmax, ymax = (moved[0].max(0)/60).astype(int), (moved[1].max(0)/60).astype(int)
+        fromx, fromy, tox, toy = 0, 0, 0, 0
+        if i%2 == 1:
+            fromx, fromy = xmin, ymin
+            tox, toy = xmax, ymax
+        else:
+            fromx, fromy = xmax, ymax
+            tox, toy = xmin, ymin
+        piece = str(board.board[fromx][fromy].piece_name)        
+        print(piece, "from", (fromx, fromy), "to", (tox, toy) )
+        board.set_piece(piece, tox, toy)
+        board.set_piece("", fromx, fromy)
         
     return game
