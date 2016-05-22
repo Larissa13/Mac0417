392ccda23e924c60107ca4e9daea0c937844fb2d
Larissa13 <lariop13@gmail.com>
2016-05-22 14:54:58 -0300

    Ep02

--- EP_02/chess/analyze.py
+++ EP_02/chess/analyze.py
@@ -6,9 +6,10 @@ def analyze_chess_game(image_directory):
     game = Game()
     plays = []
     board = Chessboard()
-    board.print_board()
+    Images = []
     for i in range(28):
         image = str(image_directory) + "/" + str (i) + ".png"
+        Images.append(image)
         f = read_gray_image(image)
         plays.append(f)
     
@@ -16,18 +17,29 @@ def analyze_chess_game(image_directory):
     for i in range(n-1):
         diff = plays[i+1] - plays[i]
         moved = np.nonzero(diff)
-        xmin, ymin = (moved[0].min(0)/60).astype(int), (moved[1].min(0)/60).astype(int)
-        xmax, ymax = (moved[0].max(0)/60).astype(int), (moved[1].max(0)/60).astype(int)
-        fromx, fromy, tox, toy = 0, 0, 0, 0
-        if i%2 == 1:
+        xmin = (moved[0].min(0)/60).astype(int)
+        xmax = (moved[0].max(0)/60).astype(int)
+        y0 = np.nonzero(diff[xmin*60:(xmin+1)*60, :])
+        y1 = np.nonzero(diff[(xmax)*60:(xmax+1)*60, :])
+        ymin = (y0[1].min()/60).astype(int)
+        ymax = (y1[1].max()/60).astype(int)
+        fromx, fromy = 0, 0
+        tox, toy = 0,0
+        if i%2==1:
             fromx, fromy = xmin, ymin
             tox, toy = xmax, ymax
         else:
             fromx, fromy = xmax, ymax
             tox, toy = xmin, ymin
-        piece = str(board.board[fromx][fromy].piece_name)        
-        print(piece, "from", (fromx, fromy), "to", (tox, toy) )
-        board.set_piece(piece, tox, toy)
-        board.set_piece("", fromx, fromy)
         
+        piece0 = str(board.board[fromx][fromy].piece_name)
+        piece1= str(board.board[tox][toy].piece_name)
+        piece_start = ChessSquare(piece0, fromx, fromy)
+        piece_stop = ChessSquare(piece1, tox, toy)
+        move = Move(Images[i+1], piece_start, piece_stop)
+        game.add_move(move)
+        board.set_piece(piece0, tox, toy)
+        board.set_piece("", fromx, fromy)
+
+
     return game
